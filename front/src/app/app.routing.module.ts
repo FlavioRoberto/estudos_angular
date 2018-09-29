a719c1f1dc228163405938a989461f9c95f22be7
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-09-29 16:41:22 -0300

    Estudos de rotas

---
+++ front/src/app/app.routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
+
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotosFormComponent } from './photos/photos-form/photos-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotosFormComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- front/src/app/photos/photos.module.ts
+++ front/src/app/photos/photos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { PhotoComponent } from './photo/photo.component';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotosFormComponent } from './photos-form/photos-form.component';
 
 @NgModule({
-  imports: [HttpClientModule],
-  declarations: [PhotoComponent, PhotoListComponent]
+  imports: [HttpClientModule, CommonModule],
+  declarations: [PhotoComponent, PhotoListComponent, PhotosFormComponent]
 })
 export class PhotosModule {}
