d73390fcae5187d8caaa3b0369a5efa81f3d8487
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-09-29 16:13:32 -0300

    Ajuste aspas duplas

--- front/src/app/photos/photos.module.ts
+++ front/src/app/photos/photos.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
   imports: [HttpClientModule],
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent]
+  declarations: [PhotoComponent, PhotoListComponent]
 })
 export class PhotosModule {}
