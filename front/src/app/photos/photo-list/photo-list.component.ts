8c6b29b5979258703a263eec5446c65fefbef4aa
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-10-02 13:56:42 -0300

    Trabalhando com loadMore 2.0

--- front/src/app/photos/photo-list/photo-list.component.ts
+++ front/src/app/photos/photo-list/photo-list.component.ts
@@ -37,7 +37,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     load() {
         this.photoService.listFromUserPaginated(this.userName, this.currentPage++)
             .subscribe(photos => {
-                this.photos.push(...photos);
+                this.photos = this.photos.concat(photos);
                 console.log(photos);
                 if (!photos.length)
                     this.hasMore = false;
