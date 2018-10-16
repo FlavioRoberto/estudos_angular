0fc79789a1bb738025c3f22e67f5bff1dc8ea3d5
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-10-16 13:35:06 -0300

    Trabalhando com autenticação

--- front/src/app/home/signin/signin.component.ts
+++ front/src/app/home/signin/signin.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router, Route } from '@angular/router';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.css']
 })
 
 export class SigninComponent implements OnInit {
-   loginForm: FormGroup;
+    private loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+    constructor(private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router) { }
 
-  }
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
 
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      userName: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        this.authService.authenticate(userName, password)
+            .subscribe(() => {
+                this.router.navigate(['user',userName]);
+            },
+                error => {
+                    console.log(error)
+                    this.loginForm.reset();
+                });
+    }
 }
