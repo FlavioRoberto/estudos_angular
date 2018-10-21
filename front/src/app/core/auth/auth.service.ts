98fb20e225c14928c7e7635910147fbcd08f816d
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-10-21 18:07:58 -0300

    Recuperando o token da requisição

--- front/src/app/core/auth/auth.service.ts
+++ front/src/app/core/auth/auth.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
-const API_URL = "http://localhost:3003/";
+const API_URL = 'http://localhost:3003/';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
-
-    authenticate(userName: string, password: string) {
-       return this.http.post(API_URL + 'user/login', {
-            userName, password
-        });
-    }
+  authenticate(userName: string, password: string) {
+    return this.http
+      .post(
+        API_URL + 'user/login',
+        {
+          userName,
+          password
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap(res => {
+          const authToken = res.headers.get('x-access-token');
+          console.log(authToken);
+        })
+      );
+  }
 }
